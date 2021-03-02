45806d89b769dab311f004167a28fdc3149a4401
Lorens León <lorensleon@gmail.com>
2021-03-02 17:27:01 +0100

    fix: correctly extract the properties that are not methods

--- src/types/Builder.ts
+++ src/types/Builder.ts
@@ -1,4 +1,5 @@
-export type NonMethodKeys<T> = { [P in keyof T]: T[P] extends Function ? never : P }[keyof T];
+export type ExcludeMethods<T> = { [P in keyof T as T[P] extends Function ? never : P]: T[P] };
+export type NonMethodKeys<T> = keyof ExcludeMethods<T>;
 
 type Setters<T, R> = { [K in NonMethodKeys<T> & string as `set${Capitalize<K>}`]: (value: T[K]) => Builder<T, R> };
 type Removers<T, R> = { [K in NonMethodKeys<T> & string as `unset${Capitalize<K>}`]: () => Builder<T, R> };
